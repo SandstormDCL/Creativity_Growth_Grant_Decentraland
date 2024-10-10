e668a602eb57f4dd0f92e9bf1e9cc14ee4557d86
Rhoooooo <142379193+Rhoooooo@users.noreply.github.com>
2024-10-10 08:15:50 +0700

    Delete Round #5 - Salt and Suger/Rhoo/Render.bat

--- Round #5 - Salt and Suger/Rhoo/Render.bat
+++
@@ -1,4 +0,0 @@
-cd "C:\Program Files\Blender Foundation\Blender 3.3"
-
-blender -b "D:\Rhoo\Blender_Hoo\Asset\Scenes\Doc_download.blend" -o "D:\Rhoo\Blender_Hoo\Asset\Scenes\Render\Doc_download_" -F PNG -f 1
-
